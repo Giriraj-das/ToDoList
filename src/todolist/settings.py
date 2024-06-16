ea3d6fbad1342d6decc60d01ca4f0118dc53a3d1
Giriraj-das <girirajdaspsdg@gmail.com>
2024-06-16 15:39:19 +0530

    deploy

--- src/todolist/settings.py
+++ src/todolist/settings.py
@@ -128,7 +128,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR.joinpath('static')
 
 AUTH_USER_MODEL = 'core.User'
 
